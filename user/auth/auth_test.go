7f0c7c29399ce346e92be973a8cb5bf471105fa1
Benjamin Radovsky <radovskyb@gmail.com>
2017-02-15 01:14:35 +1100

    create validation error test

--- user/auth/auth_test.go
+++ user/auth/auth_test.go
@@ -13,67 +13,6 @@ const (
 	testPassword = "password123" // Only use bcrypt in production database.
 )
 
-func TestCreateUser(t *testing.T) {
-	repo := datastore.NewMockRepo()
-	auth := NewAuth(repo)
-
-	u := &user.User{
-		Email:    testEmail,
-		Username: testUsername,
-		Password: testPassword,
-	}
-
-	err := auth.CreateUser(u)
-	if err != nil {
-		t.Error(err)
-	}
-
-	// Get the inserted user from the database
-	// and check that the password was hashed.
-	u, err = repo.GetByUsername(testUsername)
-	if err != nil {
-		t.Error(err)
-	}
-	if u.Password == testPassword {
-		t.Errorf("expected u.Password to be %s, got %s",
-			testPassword, u.Password)
-	}
-}
-
-func TestAuthenticateUser(t *testing.T) {
-	repo := datastore.NewMockRepo()
-	auth := NewAuth(repo)
-
-	u := &user.User{
-		Email:    testEmail,
-		Username: testUsername,
-		Password: testPassword,
-	}
-
-	err := auth.CreateUser(u)
-	if err != nil {
-		t.Error(err)
-	}
-
-	// Try to authenticate a user with correct credentials.
-	err = auth.AuthenticateUser(testUsername, testPassword)
-	if err != nil {
-		t.Error(err)
-	}
-
-	// Try to authenticate a user that doesn't exist.
-	err = auth.AuthenticateUser("invalidUsername", testPassword)
-	if err != datastore.ErrUserNotFound {
-		t.Error("expected err to be ErrUserNotFound, got %v", err)
-	}
-
-	// Try to authenticate a user with an invalid password.
-	err = auth.AuthenticateUser(testUsername, "invalidPassword")
-	if err == nil {
-		t.Error("expected err to not be nil")
-	}
-}
-
 func TestValidateUser(t *testing.T) {
 	auth := NewAuth(datastore.NewMockRepo())
 
@@ -178,5 +117,73 @@ func TestValidateUser(t *testing.T) {
 	err = auth.ValidateUser(u)
 	if err != ErrPasswordTooShort {
 		t.Errorf("expected err to be ErrInvalidPasswordLength, got %v", err)
+	}
+}
+
+func TestCreateUser(t *testing.T) {
+	repo := datastore.NewMockRepo()
+	auth := NewAuth(repo)
+
+	u := &user.User{
+		Email:    testEmail,
+		Username: testUsername,
+		Password: testPassword,
+	}
+
+	err := auth.CreateUser(u)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Get the inserted user from the database
+	// and check that the password was hashed.
+	u, err = repo.GetByUsername(testUsername)
+	if err != nil {
+		t.Error(err)
+	}
+	if u.Password == testPassword {
+		t.Errorf("expected u.Password to be %s, got %s",
+			testPassword, u.Password)
+	}
+
+	// Try to create an invalid user.
+	u.Password = ""
+	err = auth.CreateUser(u)
+	if err == nil {
+		t.Errorf("expected there to be a validation error, got %v", err)
+	}
+}
+
+func TestAuthenticateUser(t *testing.T) {
+	repo := datastore.NewMockRepo()
+	auth := NewAuth(repo)
+
+	u := &user.User{
+		Email:    testEmail,
+		Username: testUsername,
+		Password: testPassword,
+	}
+
+	err := auth.CreateUser(u)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Try to authenticate a user with correct credentials.
+	err = auth.AuthenticateUser(testUsername, testPassword)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Try to authenticate a user that doesn't exist.
+	err = auth.AuthenticateUser("invalidUsername", testPassword)
+	if err != datastore.ErrUserNotFound {
+		t.Error("expected err to be ErrUserNotFound, got %v", err)
+	}
+
+	// Try to authenticate a user with an invalid password.
+	err = auth.AuthenticateUser(testUsername, "invalidPassword")
+	if err == nil {
+		t.Error("expected err to not be nil")
 	}
 }
